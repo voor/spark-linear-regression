a350b8d47d5eb313e97b8cbb349b7d9b171f1a23
Robert Van Voorhees <rcvanvo@gmail.com>
2016-05-09 09:01:21 -0400

    Working to figure out how to start up a Spark job.

--- src/main/java/com/planetvoor/SparkLinearRegressionApplication.java
+++ src/main/java/com/planetvoor/SparkLinearRegressionApplication.java
@@ -1,12 +1,33 @@
 package com.planetvoor;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SparkLinearRegressionApplication {
+@EnableBatchProcessing
+public class SparkLinearRegressionApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SparkLinearRegressionApplication.class, args);
-	}
+
+    @Autowired
+    JobLauncher jobLauncher;
+
+    @Autowired
+    Job tweetTopHashtags;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkLinearRegressionApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("RUNNING ...");
+        jobLauncher.run(tweetTopHashtags, new JobParametersBuilder().toJobParameters());
+    }
+
 }
