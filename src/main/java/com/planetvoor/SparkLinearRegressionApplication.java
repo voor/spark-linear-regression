b8671c79460a2856695d0340a4da0f9b7bc0e308
Robert Van Voorhees <rcvanvo@gmail.com>
2016-05-08 22:18:40 -0400

    Initial creation from https://start.spring.io/

---
+++ src/main/java/com/planetvoor/SparkLinearRegressionApplication.java
@@ -0,0 +1,12 @@
+package com.planetvoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkLinearRegressionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkLinearRegressionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/planetvoor/SparkLinearRegressionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.planetvoor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SparkLinearRegressionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
